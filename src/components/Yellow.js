1720578a9bad2a83ff7e38a3a7583f76c5cdd257
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-20 11:14:39 -0700

    yellow component

---
+++ src/components/Yellow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Yellow() {
+  const style = {
+    backgroundColor: 'yellow',
+    height: '100px',
+    width: '100px'
+  };
+
+  return <div style={style}></div>;
+}
