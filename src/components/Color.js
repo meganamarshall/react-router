eda34e3eec3bb52ba42bec2739c26213950f3391
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-21 18:59:59 -0700

    changing to 'match' per the demo

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Color() {
-  const colors = ['pink', 'green', 'lightcoral', 'purple', 'lightblue', 'coral'];
+function Color({ match }) {
 
   const style = {
-    backgroundColor: colors[Math.floor(Math.random() * colors.length)],
+    backgroundColor: decodeURIComponent(match.params.color),
     height: '100px',
     width: '100px'
   };
 
   return <div style={style}></div>;
 }
+
+Color.proptypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      color: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
+
+export default Color;
